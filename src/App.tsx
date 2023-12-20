d807f9930c834d8b0725739371975e0af9c8232f
Siddharth Shah <siddharthshah1102@gmail.com>
2023-12-20 16:33:45 +0530

    added bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import "./App.css";
+import { Container } from "react-bootstrap";
 import { Navigate, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<h1>Hi</h1>} />
-      <Route path="/new" element={<h1>New</h1>} />
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
+    <Container>
+      <Routes>
+        <Route path="/" element={<h1>Hi</h1>} />
+        <Route path="/new" element={<h1>New</h1>} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
   );
 }
 
