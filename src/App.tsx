d05cd42650b8f14de76c4f4ae1ad05f28a84eb4c
Siddharth Shah <siddharth.shah@mail.com>
2024-02-01 15:59:47 +0530

    store notes and tags in state; newnote function

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
-import "./App.css";
+import { useMemo } from "react";
+import { v4 as uuidV4 } from "uuid";
 import { Container } from "react-bootstrap";
 import { Navigate, Route, Routes } from "react-router-dom";
+import useLocalStorage from "./hooks/useLocalStorage";
+import { NoteData, RawNote, Tag } from "./types";
+import "./App.css";
 
 function App() {
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", []);
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
+
+  const notesWithTags = useMemo(() => {
+    return notes.map((note) => {
+      return {
+        ...note,
+        tags: tags.filter((tag) => note.tagIds.includes(tag.id)),
+      };
+    });
+  }, [notes, tags]);
+
+  const onCreateNote = ({ tags, ...data }: NoteData) => {
+    setNotes((prevNotes) => {
+      return [...prevNotes, {
+        ...data,
+        id: uuidV4(),
+        tagIds: tags.map((t) => t.id),
+      }];
+    });
+  };
+
   return (
     <Container>
       <Routes>
